0210e3e46bedd28dd37c94075eb8798b460a0027
ProgarmerLeo <105867299+ProgarmerLeo@users.noreply.github.com>
2025-07-17 08:12:29 +0000

    class

--- Dynamic Programming/Array Description.cpp
+++ Dynamic Programming/Array Description.cpp
@@ -85,12 +85,15 @@ std::ostream& operator<<(std::ostream& fout,std::pair<T,R>&x) {
 }
 
 
-int n;
-
+int n, m;
+int graph[MAX];
 signed main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
 
     cin >> n;
+    FN(n){
+        cin >> 
+    }
 }   
 
