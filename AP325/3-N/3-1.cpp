379ba4f68b170b4b1e4bbcf395525bf359fb916d
meowmeowmeow <leo960120160@gmail.com>
2025-07-29 22:37:41 +0800

    20250729

--- AP325/3-N/3-1.cpp
+++ AP325/3-N/3-1.cpp
@@ -1,40 +1,126 @@
 # include <bits/stdc++.h>
-# define ll long long
+# define int long long
+
+# define MAX 1000005
+# define inf 10000000000
+# define mod 1000000007
+
+# define pii pair<int, int>
+# define vi vector<int>
+# define vpii vector<pii>
+
+#define pb(...) push_back(__VA_ARGS__)
+#define bg(i) i.begin()
+#define ed(i) i.end()
+#define all(i) bg(i),ed(i)
+
+# define ff first
+# define ss second
+
+#define __overload_for__(i,j,k,l,m,...) m
+#define FN_(i) FNN(_, i)
+#define FNN(i,j) for(int i = 1;i<=j;i++)
+#define FNNM(i,j,k) for(int i = j;i<=k;i++)
+#define FNNMQ(i, j, k, l) for (int i = j; i<=k; i=i+l)
+#define FN(...) __overload_for__(__VA_ARGS__,FNNMQ,FNNM,FNN, FN_)(__VA_ARGS__)
+#define FNAT(i,j) for(auto i : j)
+
+#define nl cout<<"\n"
+#define out(i) cout<<(i)
+#define outs(i) cout<<(i)<<" "
+#define outl(i) cout<<(i)<<"\n"
+#define errl cerr << '_'*10;
+
+# define set_M() int M = (L+R)/2
+# define li 2*id
+# define ri 2*id+1
+
 using namespace std;
 
-vector<int> graph[(int)1e5+5];
-int n, h[(int)1e5+5], k, inp, maximum = 0;
-
-int update(int father){
-    int total = 0;
-    int mh = 0;
-    for(int son : graph[father-1]){
-        if (h[son-1] == -1){
-            total += update(son) + h[son-1];
-        }else{
-            total += 
-        }
-    }   
+template<typename T>
+inline int ab(int x) {
+    return x >= 0 ? x : -x;
+}
+template<typename T>
+inline T mi(T a, T b) {
+    return a < b ? a : b;
+}
+template<typename T>
+inline T ma(T a, T b) {
+    return a > b ? a : b;
 }
 
-int main(){
-    memset(h, -1, sizeof(h));
-    cin >> n;
-    for (int i = 0; i<n; i++){
-        cin >> k;
-        if (k){
-            for (int j = 0; j<k; j++){
-                cin >> inp;
-                graph[i].push_back(inp);
-            }
-        }else{
-            h[i] = 0;
-        }
-    }
+//better io
+#ifdef cin
+template<class T>
+__fast_read& operator>>(__fast_read& fin,vector<T>&f) {
+    for(auto &&i : f) fin>>i;
+    return fin;
+}
+template<class T,class R>
+__fast_read& operator>>(__fast_read& fin,pair<T,R>&x) {
+    fin>>x.first>>x.second;
+    return fin;
+}
+#else
+template<class T>
+std::istream& operator>>(std::istream& fin,std::vector<T>&f) {
+    for(auto &&i : f) fin>>i;
+    return fin;
+}
+template<class T,class R>
+std::istream& operator>>(std::istream& fin,std::pair<T,R>&x) {
+    fin>>x.first>>x.second;
+    return fin;
+}
+#endif
+template<class T>
+std::ostream& operator<<(std::ostream& fout,std::vector<T>&f) {
+    for(auto &i : f) fout<<i<<" ";
+    return fout;
+}
+template<class T,class R>
+std::ostream& operator<<(std::ostream& fout,std::pair<T,R>&x) {
+    fout<<x.first<<" "<<x.second;
+    return fout;
+}
 
-    for (int i = 0; i<n; i++){
-        for(int pot : graph[i]){
-            if (h[pot-1] == -1)
+
+
+int n, k, inp, ans = 0, root[MAX];
+vi graph[MAX], high(MAX, 0);
+void dfs(int cur , int pre){
+    FNAT(nxt, graph[cur]){
+        if(nxt == pre){
+            continue;
+        }
+        dfs(nxt, cur);
+        high[cur] = ma(high[cur], high[nxt]+1);
+    }
+}
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    cin >> n;
+    FN(i, n){
+        cin >> k;
+        FN(k){
+            cin >> inp;
+            root[inp] = 1;
+            graph[i].pb(inp);
         }
     }
+    int rt;
+    FN(i, n){
+        if (root[i] == 0){
+            rt = i;
+        }
+    }
+    dfs(rt, -1);
+    FN(i, n){
+        ans+=high[i];
+    }
+    outl(rt);
+    out(ans);
+
 }
