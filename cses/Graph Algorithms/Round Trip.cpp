4f33d6163e1b3efde12d640328d060fe64677e6b
meowmeowmeowcute <leo960120160@gmail.com>
2026-03-12 22:29:19 +0800

    20260312

--- cses/Graph Algorithms/Round Trip.cpp
+++ cses/Graph Algorithms/Round Trip.cpp
@@ -1,35 +1,35 @@
 # include <bits/stdc++.h>
 # define int long long
-
+ 
 # define MAX 200005
 # define inf 100000000000000000
 # define mod 1000000007
-
+ 
 # define pii pair<int, int>
 # define vi vector<int>
 # define vpii vector<pii>
-
+ 
 # define pb(...) push_back(__VA_ARGS__)
 # define bg(i) i.begin()
 # define ed(i) i.end()
 # define all(i) bg(i),ed(i)
-
+ 
 # define ff first
 # define ss second
-
-
+ 
+ 
 # define nl cout<<"\n"
 # define out(i) cout<<(i)
 # define outs(i) cout<<(i)<<" "
 # define outl(i) cout<<(i)<<"\n"
 # define elif else if
-
+ 
 # define set_M() int M = (L+R)/2
 # define li 2*id
 # define ri 2*id+1
-
+ 
 using namespace std;
-
+ 
 // # define __overload_for__(i,j,k,l,m,...) m
 // # define FN_(i) FNN(_, i)
 // # define FNN(i,j) for(int i = 1;i<=j;i++)
@@ -37,7 +37,7 @@ using namespace std;
 // # define FNNM(i,j,k) for(int i = j;i<=k;i++)
 // # define FNNMQ(i, j, k, l) for (int i = j; i<=k; i=i+l)
 // # define FN(...) __overload_for__(__VA_ARGS__,FNNMQ,FNNM,FNN, FN_)(__VA_ARGS__)
-
+ 
 inline int ab(int x) {
     return x >= 0 ? x : -x;
 }
@@ -49,7 +49,7 @@ template<typename T>
 inline T ma(T a, T b) {
     return a > b ? a : b;
 }
-
+ 
 //better io
 # ifdef cin
 template<class T>
@@ -84,31 +84,70 @@ std::ostream& operator<<(std::ostream& fout,std::pair<T,R>&x) {
     fout<<x.first<<" "<<x.second;
     return fout;
 }
-
+ 
 int n, m, a, b;
-int father[(int)1e5+10];
-int distance[(int)1e5+10];
-bool visited[(int)1e5+10];
-vi graph[(int)1e5+10];
-
+int father[MAX];
+int dis[MAX];
+int visited[MAX];
+vi graph[MAX];
+vi cir;
+bool have_cir = 0;
+ 
+void dfs(int p, int fa){
+    visited[p] = 1;
+    father[p] = fa;
+    // cout << p << " ";
+    for (auto i:graph[p]){
+        if (i == fa)continue;
+        if(visited[i] ==1){
+            int k = p;
+            cir.pb(i);
+            while(k!=i && k!=0){
+                cir.pb(k);
+                k = father[k];
+            }
+            cir.pb(i);
+            have_cir = 1;
+            return;
+        }
+        else if (visited[i]==0 ){
+            dfs(i, p);
+            if (have_cir)return;
+        }
+    }    
+    visited[p] = 2;
+}
+ 
 signed main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);    
-
+ 
     cin >> n >> m;
     for (int i = 0; i<m; i++){
         cin >> a >> b;
         graph[a].pb(b);
         graph[b].pb(a);
     }
-
-    queue<int> q.push(1);
-    visited[1] = 1;
-    while(!q.empty()){
-
+    for (int i = 1; i<=n; i++){
+        if (visited[i] ==0){
+ 
+            dfs(i, 0);
+        }
+        if (have_cir){
+            break;
+        }
+        // cout << "\n";
     }
+    // cout << "______________\n";
+    if (have_cir){
+        cout<< cir.size() << "\n";
+        for (auto i : cir){
+            cout << i << " ";
+        }
+    }else{
+        cout << "IMPOSSIBLE";
+    }
+ 
+ 
 //dfs
 }   
-
-
-
