c4e6629ea00ca7427ddd1ae29dcfe280940001ae
meowmeowmeow <leo960120160@gmail.com>
2026-03-30 13:33:18 +0800

    20260330

---
+++ cses/Dynamic Programming/Removal Game/1d array.cpp
@@ -0,0 +1,76 @@
+# include <bits/stdc++.h>
+# define int long long
+
+# define MAX 200005
+const int INF = 0x3f3f3f3f;
+const long long LINF = 0x3f3f3f3f3f3f3f3f;
+# define mod 1000000007
+
+# define pii pair<int, int>
+# define vi vector<int>
+# define vpii vector<pii>
+
+# define pb(...) push_back(__VA_ARGS__)
+# define bg(i) i.begin()
+# define ed(i) i.end()
+# define all(i) bg(i),ed(i)
+
+# define ff first
+# define ss second
+
+
+# define nl cout<<"\n"
+# define out(i) cout<<(i)
+# define outs(i) cout<<(i)<<" "
+# define outl(i) cout<<(i)<<"\n"
+# define elif else if
+
+# define set_M() int M = (L+R)/2
+# define li 2*id
+# define ri 2*id+1
+
+using namespace std;
+
+inline int ab(int x) {
+    return x >= 0 ? x : -x;
+}
+template<typename T>
+inline T mi(T a, T b) {
+    return a < b ? a : b;
+}
+template<typename T>
+inline T ma(T a, T b) {
+    return a > b ? a : b;
+}
+
+int dp[5005];
+int graph[5005];
+
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);    
+
+    int n;
+    cin >> n;
+    int total = 0;
+    for (int i = 1; i<=n; i++){
+        cin >> graph[i];
+        total+=graph[i];
+    }
+    
+    for (int i = 1; i<=n; i++){
+        dp[i] = graph[i];
+    }
+
+    for (int step = 1; step<=n-1; step++){
+        for (int i = 1; i<=n-step; i++){
+            int j = i +step;
+            dp[i] = max(graph[i]-dp[i+1], graph[j]-dp[i]); 
+        }
+    }
+    cout << (total+dp[1])/2;
+
+}   
+
+
+
