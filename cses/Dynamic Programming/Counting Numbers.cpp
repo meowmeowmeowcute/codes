cb6f8556db0b953d1c0db0f4698245c5ea7ea25a
meowmeowmeow <leo960120160@gmail.com>
2026-04-10 15:04:52 +0800

    20260410

--- cses/Dynamic Programming/Counting Numbers.cpp
+++ cses/Dynamic Programming/Counting Numbers.cpp
@@ -43,10 +43,21 @@ inline T ma(T a, T b) {
     return a > b ? a : b;
 }
 
-
+string a, b;
 int dp[20][10][2];
 void dfs(int id, int last_dight, bool is_limit, bool is_leadzero){
-
+    int limit = a[id];
+    if(is_limit){
+        for (int i = 0; i<=limit; i++){
+            bool nxt_limit = 0;
+            bool nxt_zero = 0;
+            if (is_limit && i == limit)nxt_limit = 1;
+            if (is_leadzero && i == 0)nxt_zero = 1;
+            if (i != last_dight){
+                dfs(id+1, i, nxt_limit, nxt_zero);
+            } 
+        }
+    }
 }
 
 signed main(){
