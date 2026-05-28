c9dcce53405de79547c1bfd94b500754494f640b
meowmeowmeowcute <leo960120160@gmail.com>
2026-05-28 19:14:47 +0800

    20260528

---
+++ APCS/2026 march/Intermediate to advanced/s180.cpp
@@ -0,0 +1,60 @@
+# include <bits/stdc++.h>
+using namespace std;
+//# define int long long
+
+// # include <ext/pb_ds/assoc_container.hpp>
+// # include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// // Define a balanced tree (PBDS Tree) that supports order statistics (ranking)
+// typedef tree<
+//     int,                    // Key type
+//     null_type,              // Value type : use null_type for a set
+//     less<int>,              // Comparison method
+//     rb_tree_tag,            // Tree type : Red-Black Tree
+//     tree_order_statistics_node_update // Enables order statistics support
+// > indexed_set;
+
+# define MAX 200005
+const int INF = 0x3f3f3f3f;
+const long long LINF = 0x3f3f3f3f3f3f3f3f;
+# define mod 1000000007
+
+# define pii pair<int, int>
+# define vi vector<int>
+# define vpii vector<pii>
+
+# define pb(...) push_back(__VA_ARGS__)
+# define bg(i) i.begin()
+# define ed(i) i.end()
+# define all(i) bg(i),ed(i)
+
+# define ff first
+# define ss second
+
+# define nl cout<<"\n"
+# define out(i) cout<<(i)
+# define outs(i) cout<<(i)<<" "
+# define outl(i) cout<<(i)<<"\n"
+# define elif else if
+
+# define set_M() int M = (L+R)/2
+# define li 2*id
+# define ri 2*id+1
+
+inline int ab(int x) {
+    return x >= 0 ? x : -x;
+}
+template<typename T>
+inline T mi(T a, T b) {
+    return a < b ? a : b;
+}
+template<typename T>
+inline T ma(T a, T b) {
+    return a > b ? a : b;
+}
+
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);    
+
+};   
